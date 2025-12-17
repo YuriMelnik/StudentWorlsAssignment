07156959868101363029b29b7500f4865333dc89
7289772 <7289772@gmail.com>
2025-12-17 20:04:02 +0300

    изменены Icon & favicon

--- StudentWorlsAssignment/Form1.Designer.cs
+++ StudentWorlsAssignment/Form1.Designer.cs
@@ -54,10 +54,11 @@
             buttonLosdArchive.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             buttonLosdArchive.BackColor = SystemColors.ButtonHighlight;
             buttonLosdArchive.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
-            buttonLosdArchive.Location = new Point(703, 117);
+            buttonLosdArchive.Location = new Point(1306, 250);
+            buttonLosdArchive.Margin = new Padding(6);
             buttonLosdArchive.Name = "buttonLosdArchive";
             buttonLosdArchive.RightToLeft = RightToLeft.Yes;
-            buttonLosdArchive.Size = new Size(101, 56);
+            buttonLosdArchive.Size = new Size(188, 119);
             buttonLosdArchive.TabIndex = 1;
             buttonLosdArchive.Text = "Загрузить архив \r\n    с работами";
             buttonLosdArchive.UseVisualStyleBackColor = false;
@@ -68,9 +69,10 @@
             panelPreview.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             panelPreview.BackColor = SystemColors.ControlLightLight;
             panelPreview.BackgroundImageLayout = ImageLayout.Stretch;
-            panelPreview.Location = new Point(12, 12);
+            panelPreview.Location = new Point(22, 26);
+            panelPreview.Margin = new Padding(6);
             panelPreview.Name = "panelPreview";
-            panelPreview.Size = new Size(418, 373);
+            panelPreview.Size = new Size(776, 796);
             panelPreview.TabIndex = 0;
             // 
             // dataGridViewStudentvsMark
@@ -80,12 +82,13 @@
             dataGridViewStudentvsMark.BackgroundColor = SystemColors.ControlLightLight;
             dataGridViewStudentvsMark.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridViewStudentvsMark.Columns.AddRange(new DataGridViewColumn[] { Student, Mark });
-            dataGridViewStudentvsMark.Location = new Point(436, 201);
+            dataGridViewStudentvsMark.Location = new Point(810, 429);
+            dataGridViewStudentvsMark.Margin = new Padding(6);
             dataGridViewStudentvsMark.MultiSelect = false;
             dataGridViewStudentvsMark.Name = "dataGridViewStudentvsMark";
             dataGridViewStudentvsMark.RowHeadersWidth = 40;
             dataGridViewStudentvsMark.SelectionMode = DataGridViewSelectionMode.CellSelect;
-            dataGridViewStudentvsMark.Size = new Size(368, 284);
+            dataGridViewStudentvsMark.Size = new Size(683, 606);
             dataGridViewStudentvsMark.TabIndex = 2;
             dataGridViewStudentvsMark.CellClick += DataGridViewStudentvsMark_CellContentClick;
             // 
@@ -109,9 +112,10 @@
             textBox_NeuroTask.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             textBox_NeuroTask.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
             textBox_NeuroTask.ForeColor = SystemColors.ActiveCaption;
-            textBox_NeuroTask.Location = new Point(436, 12);
+            textBox_NeuroTask.Location = new Point(810, 26);
+            textBox_NeuroTask.Margin = new Padding(6);
             textBox_NeuroTask.Name = "textBox_NeuroTask";
-            textBox_NeuroTask.Size = new Size(343, 29);
+            textBox_NeuroTask.Size = new Size(634, 50);
             textBox_NeuroTask.TabIndex = 4;
             textBox_NeuroTask.Text = "Задание для нейропомощника";
             // 
@@ -120,9 +124,10 @@
             label1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             label1.AutoSize = true;
             label1.ForeColor = SystemColors.ButtonShadow;
-            label1.Location = new Point(675, 0);
+            label1.Location = new Point(1254, 0);
+            label1.Margin = new Padding(6, 0, 6, 0);
             label1.Name = "label1";
-            label1.Size = new Size(131, 15);
+            label1.Size = new Size(264, 32);
             label1.TabIndex = 7;
             label1.Text = "(С) Мельник Ю.В. 2025";
             // 
@@ -131,10 +136,11 @@
             labelArchivFileName.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             labelArchivFileName.AutoSize = true;
             labelArchivFileName.ForeColor = SystemColors.ControlDarkDark;
-            labelArchivFileName.Location = new Point(436, 117);
-            labelArchivFileName.MaximumSize = new Size(260, 60);
+            labelArchivFileName.Location = new Point(810, 250);
+            labelArchivFileName.Margin = new Padding(6, 0, 6, 0);
+            labelArchivFileName.MaximumSize = new Size(483, 128);
             labelArchivFileName.Name = "labelArchivFileName";
-            labelArchivFileName.Size = new Size(260, 60);
+            labelArchivFileName.Size = new Size(476, 128);
             labelArchivFileName.TabIndex = 9;
             labelArchivFileName.Text = "имя архива - загрузите архив с ответами!\r\nВ СДО ИДО на странице задания выберите \"Сохранить все ответы\". Загрузите этот архив .\r\n";
             // 
@@ -144,9 +150,10 @@
             buttonAiReview.BackColor = SystemColors.GradientInactiveCaption;
             buttonAiReview.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             buttonAiReview.ForeColor = SystemColors.HotTrack;
-            buttonAiReview.Location = new Point(703, 76);
+            buttonAiReview.Location = new Point(1306, 162);
+            buttonAiReview.Margin = new Padding(6);
             buttonAiReview.Name = "buttonAiReview";
-            buttonAiReview.Size = new Size(99, 35);
+            buttonAiReview.Size = new Size(184, 75);
             buttonAiReview.TabIndex = 10;
             buttonAiReview.Text = "AI-проверка";
             buttonAiReview.UseVisualStyleBackColor = false;
@@ -158,18 +165,20 @@
             buttonProperties.BackgroundImage = Properties.Resources.settings100;
             buttonProperties.BackgroundImageLayout = ImageLayout.Stretch;
             buttonProperties.Image = Properties.Resources.settings;
-            buttonProperties.Location = new Point(911, 15);
+            buttonProperties.Location = new Point(1692, 32);
+            buttonProperties.Margin = new Padding(6);
             buttonProperties.Name = "buttonProperties";
-            buttonProperties.Size = new Size(25, 22);
+            buttonProperties.Size = new Size(46, 47);
             buttonProperties.TabIndex = 11;
             buttonProperties.UseVisualStyleBackColor = false;
             // 
             // rbNoHighlight
             // 
             rbNoHighlight.AutoSize = true;
-            rbNoHighlight.Location = new Point(147, -2);
+            rbNoHighlight.Location = new Point(273, -4);
+            rbNoHighlight.Margin = new Padding(6);
             rbNoHighlight.Name = "rbNoHighlight";
-            rbNoHighlight.Size = new Size(109, 19);
+            rbNoHighlight.Size = new Size(215, 36);
             rbNoHighlight.TabIndex = 12;
             rbNoHighlight.Text = "без подсветки";
             rbNoHighlight.UseVisualStyleBackColor = true;
@@ -183,9 +192,11 @@
             groupBoxHighLighting.Controls.Add(rbNoHighlight);
             groupBoxHighLighting.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             groupBoxHighLighting.ForeColor = SystemColors.ControlLightLight;
-            groupBoxHighLighting.Location = new Point(566, 176);
+            groupBoxHighLighting.Location = new Point(1051, 375);
+            groupBoxHighLighting.Margin = new Padding(6);
             groupBoxHighLighting.Name = "groupBoxHighLighting";
-            groupBoxHighLighting.Size = new Size(370, 19);
+            groupBoxHighLighting.Padding = new Padding(6);
+            groupBoxHighLighting.Size = new Size(687, 41);
             groupBoxHighLighting.TabIndex = 13;
             groupBoxHighLighting.TabStop = false;
             groupBoxHighLighting.Text = "Подсветка синтаксиса";
@@ -193,9 +204,10 @@
             // rbPython
             // 
             rbPython.AutoSize = true;
-            rbPython.Location = new Point(298, -2);
+            rbPython.Location = new Point(553, -4);
+            rbPython.Margin = new Padding(6);
             rbPython.Name = "rbPython";
-            rbPython.Size = new Size(64, 19);
+            rbPython.Size = new Size(126, 36);
             rbPython.TabIndex = 14;
             rbPython.Text = "Python";
             rbPython.UseVisualStyleBackColor = true;
@@ -205,9 +217,10 @@
             // 
             rbCSharp.AutoSize = true;
             rbCSharp.Checked = true;
-            rbCSharp.Location = new Point(258, -2);
+            rbCSharp.Location = new Point(479, -4);
+            rbCSharp.Margin = new Padding(6);
             rbCSharp.Name = "rbCSharp";
-            rbCSharp.Size = new Size(39, 19);
+            rbCSharp.Size = new Size(74, 36);
             rbCSharp.TabIndex = 13;
             rbCSharp.TabStop = true;
             rbCSharp.Text = "C#";
@@ -217,16 +230,17 @@
             // checkedListBoxFiles
             // 
             checkedListBoxFiles.FormattingEnabled = true;
-            checkedListBoxFiles.Location = new Point(12, 391);
+            checkedListBoxFiles.Location = new Point(22, 834);
+            checkedListBoxFiles.Margin = new Padding(6);
             checkedListBoxFiles.Name = "checkedListBoxFiles";
-            checkedListBoxFiles.Size = new Size(418, 94);
+            checkedListBoxFiles.Size = new Size(773, 184);
             checkedListBoxFiles.TabIndex = 14;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(816, 497);
+            ClientSize = new Size(1515, 1060);
             Controls.Add(checkedListBoxFiles);
             Controls.Add(groupBoxHighLighting);
             Controls.Add(buttonProperties);
@@ -238,6 +252,7 @@
             Controls.Add(buttonLosdArchive);
             Controls.Add(panelPreview);
             Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(6);
             Name = "Form1";
             Text = "Помощник  проверки заданий";
             ((System.ComponentModel.ISupportInitialize)dataGridViewStudentvsMark).EndInit();
